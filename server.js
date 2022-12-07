a3e7f850e527094225a0ab7381b7979196bed900
minhtruc <truc0979454023@gmail.com>
2022-12-08 00:30:05 +0700

    commit

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ mongoose.connect(
   }
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome to Web sale" });
 });
